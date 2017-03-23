3a65a19606d43674c79d1808e2572fa6bc5e3611
jonmitten <jonmitten@gmail.com>
2017-03-23 15:57:35 -0700

    updates browser cache to false

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -13,7 +13,12 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        just_wait()
+        self.profile = webdriver.FirefoxProfile()
+        self.profile.set_preference("browser.cache.disk.enable", False)
+        self.profile.set_preference("browser.cache.memory.enable", False)
+        self.profile.set_preference("browser.cache.offline.enable", False)
+        self.profile.set_preference("network.http.use-cache", False)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -35,7 +40,6 @@ class NewVisitorTest(unittest.TestCase):
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-
         # She types "Buy Peacock Feathers" into a text box (Edith's
         # hobby is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
@@ -43,11 +47,11 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and the new page lists
         # " 1 : Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-
-        just_wait()
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        just_wait()
+        # element = self.browser.find_element_by_tag_name('table')
+        # source_code = element.get_attribute("outerHTML")
+        # print("\n\n{}\n\n".format(source_code))
         self.assertIn(
             '1: Buy peacock feathers',
             [row.text for row in rows]
@@ -56,17 +60,11 @@ class NewVisitorTest(unittest.TestCase):
 
         # There is still a text box inviting her to add another item
         # She enteres "Use peacock feathers to make a fly"
-        just_wait()
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_elements_by_tag_name('tr')
-        just_wait()
-        self.assertIn(
-            '1: Buy peacock feathers',
-            [row.text for row in rows]
-        )
-        just_wait()
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertIn(
             '2: Use peacock feathers to make a fly',
             [row.text for row in rows]
@@ -79,8 +77,6 @@ class NewVisitorTest(unittest.TestCase):
 
         # She visits the URL - her to-do list is still there
 
-        def just_wait(secs=120):
-            self.browser.implicitly_wait(secs)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
